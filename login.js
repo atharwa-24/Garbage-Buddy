70445cee69c36a48d41b6eaa9bc76a6dac5fffc7
Restyled.io <commits@restyled.io>
2021-02-20 21:04:11 +0000

    Restyled by clang-format

--- login.js
+++ login.js
@@ -2,14 +2,10 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
 
-    
-
     var user = firebase.auth().currentUser;
 
-    if(user != null){
-       window.location = "index.html";
-
-       
+    if (user != null) {
+      window.location = "index.html";
     }
 
   } else {
@@ -17,29 +13,28 @@ firebase.auth().onAuthStateChanged(function(user) {
 
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
   }
 });
 
-function login(){
+function login() {
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  firebase.auth()
+      .signInWithEmailAndPassword(userEmail, userPass)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
 
-    window.alert("Error : " + errorMessage);
+        window.alert("Error : " + errorMessage);
 
-    // ...
-  });
-
+        // ...
+      });
 }
 
-function logout(){
+function logout() {
   firebase.auth().signOut();
-    window.location = "login.html";
-
+  window.location = "login.html";
 }
